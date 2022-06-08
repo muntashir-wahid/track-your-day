47783e9a52fece1874172e3e43923e3ade71532e
muntashir-wahid <muntashir.wahid@yahoo.com>
2022-06-08 10:50:34 +0600

    Updates

---
+++ precache-manifest.156f761d056f556f4cb5438bbce7e947.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b78a10dc9beed2cb6dd",
+    "url": "/track-your-day/static/css/main.afe4f321.chunk.css"
+  },
+  {
+    "revision": "8b78a10dc9beed2cb6dd",
+    "url": "/track-your-day/static/js/main.8b78a10d.chunk.js"
+  },
+  {
+    "revision": "e68b5b8fea6fb46ef66d",
+    "url": "/track-your-day/static/js/1.e68b5b8f.chunk.js"
+  },
+  {
+    "revision": "8da7461dc33857931c67",
+    "url": "/track-your-day/static/js/runtime~main.8da7461d.js"
+  },
+  {
+    "revision": "cb093124aafed146aabca7cb3e3a5d9a",
+    "url": "/track-your-day/index.html"
+  }
+];
